3eee0efdb9e44ee39fd0dd8aa98bcbac035d3901
Johnny Disla <johnnyd.26@hotmail.com>
2022-03-07 20:25:54 -0400

    Initial commit

---
+++ LOGIN.cs
@@ -0,0 +1,34 @@
+using System.Data.SqlClient;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+
+       
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpass_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.UseSystemPasswordChar = true;
+        }
+    }
+}
